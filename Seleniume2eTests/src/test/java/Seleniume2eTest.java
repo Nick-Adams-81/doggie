bbaa190bedb74113fce73322af1ea3cf49cde6c0
Nick Adams <nick.pappas.adams@gmail.com>
2023-02-21 14:47:35 -0600

    added first e2e testing

--- Seleniume2eTests/src/test/java/Seleniume2eTest.java
+++ Seleniume2eTests/src/test/java/Seleniume2eTest.java
@@ -1,3 +1,5 @@
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,7 +11,52 @@ public class Seleniume2eTest {
         driver.get("https://www.saucedemo.com/");
 
         driver.manage().window().maximize();
-        Thread.sleep(3000);
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("login-button")).click();
+        String productsHeader = driver.findElement(By.className("title")).getText();
+        Assert.assertEquals("PRODUCTS", productsHeader);
+        System.out.println("title header test passed");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("item_4_title_link")).click();
+        String productHeader = driver.findElement(By.className("inventory_details_name")).getText();
+        Assert.assertEquals("Sauce Labs Backpack", productHeader);
+        Thread.sleep(2000);
+        System.out.println("backpack header test passed");
+
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("back-to-products")).click();
+
+        driver.findElement(By.className("shopping_cart_link")).click();
+        String cartItem = driver.findElement(By.className("inventory_item_name")).getText();
+        Assert.assertEquals("Sauce Labs Backpack", cartItem);
+        System.out.println("added item to cart test passed");
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("continue-shopping")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("remove-sauce-labs-backpack")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(By.id("add-to-cart-sauce-labs-bike-light")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.className("shopping_cart_link")).click();
+        String bikeLight = driver.findElement(By.className("inventory_item_name")).getText();
+        Assert.assertEquals("Sauce Labs Bike Light", bikeLight);
+        System.out.println("removed item and added new item to cart test passed");
+        Thread.sleep(2000);
+        driver.findElement(By.id("continue-shopping")).click();
+
+        Thread.sleep(2000);
+        driver.findElement(By.id("remove-sauce-labs-bike-light")).click();
+
+        Thread.sleep(2000);
         driver.quit();
     }
 }
